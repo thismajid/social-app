abaeab3a844bff60f64bb22cd5e3761ce8e1497d
thismajid <mkhorshidian72@gmail.com>
2022-03-24 10:43:09 +0430

    add form component

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const url = "http://localhost:3001/posts";
+const url = "http://localhost:3001/api/posts";
 
 export const fetchPosts = () => axios.get(url);
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,6 +1,14 @@
+import { useSelector } from "react-redux";
+
 import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+
   return (
     <h1>
       <Post />
